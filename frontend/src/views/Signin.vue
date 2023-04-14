1ed34689e1aa6baa708fca95f5212ad00e010e7b
Nikhil Vemula <srvemu@iu.edu>
2023-04-14 19:15:28 -0400

    Add sign in page

--- frontend/src/views/Signin.vue
+++ frontend/src/views/Signin.vue
@@ -1,11 +1,53 @@
 <script>
 export default {
-  name: 'Signin'
+  name: 'Signin',
+  data() {
+    return {
+      'isFormValid': false,
+      'email': '',
+      'emailRules': [
+        value => {
+          if (value) return true
+
+          return 'E-mail is required.'
+        },
+        value => {
+          if (/.+@.+\..+/.test(value)) return true
+
+          return 'E-mail must be valid.'
+        },
+      ],
+      'password': '',
+      'passwordRules':[
+        value => {
+          if (value) return true
+
+          return 'Password is required.'
+        },
+      ],
+    }
+  },
+  methods: {
+    async submit (event) {
+      alert();
+    }
+  }
 }
 </script>
 
 <template>
   <v-main class="bg-grey-lighten-3">
-    <h1>Sign in</h1>
+    <div class="d-flex align-center justify-center" style="height: 50vh">
+        <v-sheet width="400" class="mx-auto pa-4" rounded="lg">
+            <v-form @submit.prevent="submit" v-model="isFormValid">
+                <v-text-field v-model="email" :rules="emailRules" label="E-mail"></v-text-field>
+                <v-text-field v-model="password" :rules="passwordRules" label="Password"></v-text-field>
+                <v-btn :disabled="!isFormValid" type="submit" color="primary" block class="mt-2">Sign in</v-btn>
+            </v-form>
+            <div class="mt-2">
+                <p class="text-body-2">Don't have an account? <a href="/signup">Sign Up</a></p>
+            </div>
+        </v-sheet>
+    </div>
   </v-main>
 </template>
